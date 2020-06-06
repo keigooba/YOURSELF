e2803642161038cc1b02692479c5a26abdc1aec0
keigooba <keigo@keigoobanoMacBook-Pro.local>
2020-06-06 14:52:32 +0900

    contanct

--- PHP/function.php
+++ PHP/function.php
@@ -6,23 +6,6 @@
 ini_set('log_errors','on');
 //ログの出力ファイルを指定
 ini_set('error_log','php.log');
-// =============
-// メール送信API
-require 'vendor/autoload.php';
-
-$from = new SendGrid\Email(null, "test@example.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "test@example.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
 //================================
 // デバッグ
 //================================
