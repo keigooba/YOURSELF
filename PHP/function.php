dc529936300cb579b29aaded014174751526fe1f
keigooba <keigo@MacBook-Pro.local>
2020-07-05 12:32:58 +0900

    デバッグモード変更

--- PHP/function.php
+++ PHP/function.php
@@ -6,7 +6,8 @@
 ini_set('log_errors','on');
 //ログの出力ファイルを指定
 ini_set('error_log','php.log');
-
+//デバッグモードへ切り替え
+ini_set( 'display_errors', 1 );
 // 時間設定
 date_default_timezone_set('Asia/Tokyo');
 //================================
@@ -678,13 +679,13 @@ function uploadImg($file, $key){
       var_dump('kokoha2');
       // $file['mime']の値はブラウザ側で偽装可能なので、MIMEタイプを自前でチェックする
       // exif_imagetype関数は「IMAGETYPE_GIF」「IMAGETYPE_JPEG」などの定数を返す
-      // $type = @exif_imagetype($file['tmp_name']);
-      // var_dump('kokomade1');
-      // if (!in_array($type, [IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG], true)) { // 第三引数にはtrueを設定すると厳密にチェックしてくれるので必ずつける
-      //   var_dump('kokomade2');
-      //     throw new RuntimeException('画像形式が未対応です');
-      //   var_dump('kokomade3');
-      // }
+      $type = @exif_imagetype($file['tmp_name']);
+      var_dump('kokomade1');
+      if (!in_array($type, [IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG], true)) { // 第三引数にはtrueを設定すると厳密にチェックしてくれるので必ずつける
+        var_dump('kokomade2');
+          throw new RuntimeException('画像形式が未対応です');
+        var_dump('kokomade3');
+      }
       var_dump('kokoha3');
       // ファイルデータからSHA-1ハッシュを取ってファイル名を決定し、ファイルを保存する
       // ハッシュ化しておかないとアップロードされたファイル名そのままで保存してしまうと同じファイル名がアップロードされる可能性があり、
